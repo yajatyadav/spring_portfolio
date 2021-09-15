8fbf7e8e994b9dfb5c75390498faedf383b3570e
Yajat <yyajat@gmail.com>
2021-09-15 08:52:03 -0700

    Updated menu.html and nav bar.

--- src/main/java/com/example/sping_portfolio/controllers/minilab_3.java
+++ src/main/java/com/example/sping_portfolio/controllers/minilab_3.java
@@ -1,22 +1,39 @@
 package com.example.sping_portfolio.controllers;
 
 public class minilab_3 {
-    static int unread_email = 0;
-    static int important_email = 0;
+     int unread_email = 0;
+     int important_email = 0;
+
+     public minilab_3(int unread, int important){
+         unread_email = unread;
+         important_email = important;
+
+     }
+
+      void Unread_Checker() {
+         if (!(unread_email <= 0)) {
+             System.out.println("You have unread email");
+
+         } else {
+             System.out.println("You have no unread email");
+         }
+     }
+
+     void Important_Checker() {
+
+         if (!(important_email <= 0)) {
+             System.out.println("You have important email");
+
+         } else {
+             System.out.println("You have no important email");
+         }
+     }
 
     public static void main(String[] args) {
-        if (!(unread_email <= 0)) {
-            System.out.println("You have unread email");
 
-        } else {
-            System.out.println("You have no unread email");
-        }
+         minilab_3 User = new minilab_3(5, 0);
+         User.Unread_Checker();
+         User.Important_Checker();
 
-        if (!(important_email <= 0)) {
-            System.out.println("You have important email");
-
-        } else {
-            System.out.println("You have no important email");
-        }
     }
 }
