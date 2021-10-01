cc0b9ae3c15afd8b7dc0f8ba0b738733c33cc71f
florayuan18 <flora.yuan18@gmail.com>
2021-10-01 08:29:09 -0700

    updated contact info

--- src/main/java/com/example/sping_portfolio/controllers/floraalgorithm.java
+++ src/main/java/com/example/sping_portfolio/controllers/floraalgorithm.java
@@ -5,13 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
-public class Floraabout {
-    @GetMapping("/floraabout")
-    public String flora_about(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-
+@Controller
+public class floraalgorithm {
+    @GetMapping("/floraalgorithm")
+    public String floraalgorithm(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "floraabout";
+        return "floraalgorithm";
     }
 }
 
