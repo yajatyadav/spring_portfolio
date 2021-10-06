dba3ecfd7d0592aee35ea2b4a776b8ce11fec0a1
wiz124 <hli16492@gmail.com>
2021-10-06 08:39:47 -0700

    pwetty pwetty pwage, uwu owu uwo uwo uwo uwo uwo uwo >u<

--- src/main/java/com/example/sping_portfolio/controllers/image.java
+++ src/main/java/com/example/sping_portfolio/controllers/image.java
@@ -3,16 +3,32 @@ package com.example.sping_portfolio.controllers;
  * Web Content with Spring MVCSpring Example: https://spring.io/guides/gs/serving-web-con
  */
 
-import org.springframework.stereotype.Controller;
+
+import org.springframework.stereotype.Component;
+import  org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.stream.LongStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class image {
     @GetMapping("/images")
-    public String Images(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String Images(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model, HttpServletRequest request,
+                         HttpServletResponse response) throws IOException{
         model.addAttribute("name", name);
+
+
+
+
         return "images";
     }
 }
