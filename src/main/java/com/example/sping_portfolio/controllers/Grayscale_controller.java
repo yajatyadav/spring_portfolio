7b95dce16d17ef63c953c1de1d7a373239e34cc2
Devam Shri <devamshri@gmail.com>
2021-10-13 08:44:39 -0700

    scuffed up stuff (do not pull)

--- src/main/java/com/example/sping_portfolio/controllers/Grayscale_controller.java
+++ src/main/java/com/example/sping_portfolio/controllers/Grayscale_controller.java
@@ -38,22 +38,22 @@
 //                int green = (int) (c.getGreen() * 0.587);
 //                int blue = (int) (c.getBlue() * 0.114);
 //                Color newColor = new Color(red + green + blue,
-//
+
 //                        red + green + blue, red + green + blue);
-//
+
 //                file.setRGB(j, i, newColor.getRGB());
 //            }
 //        }
-//
-//
-////        File output = new File("test" + ".jpg");
-////        ImageIO.write(file, "jpg", output);
-//
+
+
+//        File output = new File("test" + ".jpg");
+//        ImageIO.write(file, "jpg", output);
+
 //        return file;
-//
+
 //    }
-//
-//
+
+
 //    public static void image(BufferedImage file) {
 //        JFrame frame = new JFrame();
 //        Image dimg = file.getScaledInstance(file.getWidth(), file.getHeight(),
@@ -66,43 +66,41 @@
 //                (JFrame.EXIT_ON_CLOSE);
 //        frame.pack();
 //        frame.setVisible(true);
-//
+
 //    }
-//
-//
-//
-//
-//
-//
-//
-//
-//////            return output.getAbsoluteFile().getPath();
-////        return image;
-//
-//
-////
-////
+
+
+
+
+
+
+
+
+//            return output.getAbsoluteFile().getPath();
+//        return image;
+
+
 //        public static List<String> ImageList() {
 //            List<String> images = new ArrayList<>();
 //            images.add("/images/donuts.jpg");
 //            images.add("/images/milk.jpeg");
 //            images.add("images/pizza.jpg");
 //            images.add("/images/toastcat.jpg");
-//
+
 //            return images;
 //        }
-//
+
 //        public static List<BufferedImage> GrayImageList () throws IOException {
 //            List<BufferedImage> grayimages = new ArrayList<>();
 //            grayimages.add(Grayscale("C:\\Users\\Yajat\\IdeaProjects\\spring_portfolio\\src\\main\\resources\\static\\images\\donuts.jpg"));
 //            grayimages.add(Grayscale("C:\\Users\\Yajat\\IdeaProjects\\spring_portfolio\\src\\main\\resources\\static\\images\\milk.jpeg"));
 //            grayimages.add(Grayscale("C:\\Users\\Yajat\\IdeaProjects\\spring_portfolio\\src\\main\\resources\\static\\images\\pizza.jpg"));
 //            grayimages.add(Grayscale("C:\\Users\\Yajat\\IdeaProjects\\spring_portfolio\\src\\main\\resources\\static\\images\\toastcat.jpg"));
-//
+
 //            return grayimages;
-//
+
 //        }
-//
+
 //        public static List<String> save_image() throws IOException {
 //        List<BufferedImage> grayimages = GrayImageList();
 //        ArrayList<String> filepaths = new ArrayList<String>();
@@ -114,30 +112,20 @@
 //        }
 //            return filepaths;
 //    }
-//
+
 //        @GetMapping("/images")
 //        public String Grayscale (Model model) throws IOException {
 //            model.addAttribute("image_list", ImageList());
 //            model.addAttribute("gray_image_list", save_image());
 //            return "/images";
-//
+
 //        }
-//
+
 //        public static void main(String[] args) throws IOException {
 //            BufferedImage image2 = Grayscale("C:\\Users\\Yajat\\IdeaProjects\\spring_portfolio\\src\\main\\resources\\static\\images\\donuts.jpg");
-////            image(image2);
+//            image(image2);
 //            System.out.println(GrayImageList());
 //            List<String> filepath = save_image();
 //            System.out.println(filepath);
-//
-//
-//
-//
 //        }
-//
 //}
-//
-//
-//
-//
-//
